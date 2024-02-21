e7a0bb26bcade76a197ae8d95961cdf5110e08d4
Alexander <sashok20053@gmail.com>
2024-02-21 14:59:06 +0200

    Update 2. Sierpinski_triangle.py

--- 2. Operators, conditional operator, loop operators/2. Sierpinski_triangle.py
+++ 2. Operators, conditional operator, loop operators/2. Sierpinski_triangle.py
@@ -1,35 +1,12 @@
-def Max_spaces(P, N):
-    spaces = 0
-    for i in range(len(P[N])):
-        spaces += 1
-    return spaces
-
-
 def Number_of_spaces(P, N):
     spaces = 0
     for i in range(len(P[N])):
         spaces+=1
     return int(spaces)
 
-def degree(N):
-    m = 1
-    while m <= N:
-        if m == N:
-            break
-        else:
-            m = m * 2
-    return m
-
-
-
 N = int(input("Input natural value of N: "))
-m=degree(N)
-if(m>N): N=m//2
-elif N>=2: N=m
-else: exit(-5)
-print(N)
+N=2**N
 P = []
-
 for i in range(0, N):
     row = [1] * (i + 1)
     for j in range(1, i + 1):
@@ -37,7 +14,7 @@ for i in range(0, N):
             row[j] = P[i - 1][j - 1] + P[i - 1][j]
     P.append(row)
 
-maxSpaces = Max_spaces(P, N - 1)
+maxSpaces = Number_of_spaces(P, N - 1)
 for i in range(N):
     print(" " * (maxSpaces - Number_of_spaces(P, i)), end='')
     for j in range(i + 1):
